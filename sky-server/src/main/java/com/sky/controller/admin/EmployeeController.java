70039272df92116990ea4ef6d8bab3cf48508c53
LabMemNo.04 <1051011527@qq.com>
2023-08-28 17:36:11 +0800

    启用禁用员工分账号业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -33,6 +33,20 @@ public class EmployeeController {
     private JwtProperties jwtProperties;
 
     /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用员工账号")
+    public Result startOrStop(@PathVariable Integer status,Long id){
+        log.info("启用禁用员工账号：{},{}",status,id);
+        employeeService.startOrStop(status,id);
+        return Result.success();
+    }
+
+    /**
      * 员工分页查询
      * @param employeePageQueryDTO
      * @return
