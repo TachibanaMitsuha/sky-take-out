8f317aff723bfc9eeb592602872706a229bd1653
LabMemNo.04 <1051011527@qq.com>
2023-08-29 17:02:46 +0800

    新增菜品业务代码实现

---
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -0,0 +1,16 @@
+package com.sky.service;
+
+import com.sky.dto.DishDTO;
+
+/**
+ * @author makise
+ * @version 1.0
+ */
+public interface DishService {
+
+    /**
+     *新增菜品和对应的口味
+     * @param dishDTO
+     */
+    public void saveWithFlavor(DishDTO dishDTO);
+}
